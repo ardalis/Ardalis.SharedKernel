c19844b3150e74385da466d032b0b4cf48948eb1
Steve Smith <steve@kentsmiths.com>
2023-07-25 17:36:18 -0400

    Add icon Update build props Update package properties in proj file Update comment

--- src/Ardalis.SharedKernel/EntityBase.cs
+++ src/Ardalis.SharedKernel/EntityBase.cs
@@ -4,6 +4,8 @@ namespace Ardalis.SharedKernel;
 
 /// <summary>
 /// A base class for DDD Entities. Includes support for domain events dispatched post-persistence.
+/// If you prefer GUID Ids, change it here.
+/// If you need to support both GUID and int IDs, change to EntityBase&lt;TId&gt; and use TId as the type for Id.
 /// </summary>
 public abstract class EntityBase
 {
