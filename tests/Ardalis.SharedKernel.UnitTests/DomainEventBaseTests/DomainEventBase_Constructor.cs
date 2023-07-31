d54ca8d3018dd9d9989e0d6973e71ee905fb75eb
Seth Cleaver <seth@cleaver.id.au>
2023-07-31 12:34:57 +1000

    Add tests

---
+++ tests/Ardalis.SharedKernel.UnitTests/DomainEventBaseTests/DomainEventBase_Constructor.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Ardalis.SharedKernel.UnitTests.DomainEventBaseTests;
+
+public class DomainEventBase_Constructor
+{
+
+  private class TestDomainEvent : DomainEventBase { }
+
+  [Fact]
+  public void SetsDateOccurredToCurrentDateTime()
+  {
+    // Arrange
+    var beforeCreation = DateTime.UtcNow;
+
+    // Act
+    var domainEvent = new TestDomainEvent();
+
+    // Assert
+    domainEvent.DateOccurred.Should().BeOnOrAfter(beforeCreation);
+    domainEvent.DateOccurred.Should().BeOnOrBefore(DateTime.UtcNow);
+  }
+}
