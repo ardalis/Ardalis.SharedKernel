632de4387b0621e56c6d943029161df697662b04
Steve Smith <steve@kentsmiths.com>
2023-11-10 13:24:44 -0500

    Add support for typed IDs

--- src/Ardalis.SharedKernel/EntityBase.cs
+++ src/Ardalis.SharedKernel/EntityBase.cs
@@ -1,21 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Ardalis.SharedKernel;
+﻿namespace Ardalis.SharedKernel;
 
 /// <summary>
 /// A base class for DDD Entities. Includes support for domain events dispatched post-persistence.
 /// If you prefer GUID Ids, change it here.
 /// If you need to support both GUID and int IDs, change to EntityBase&lt;TId&gt; and use TId as the type for Id.
 /// </summary>
-public abstract class EntityBase
+public abstract class EntityBase : HasDomainEventsBase
 {
   public int Id { get; set; }
+}
 
-  private List<DomainEventBase> _domainEvents = new();
-  [NotMapped]
-  public IEnumerable<DomainEventBase> DomainEvents => _domainEvents.AsReadOnly();
-
-  protected void RegisterDomainEvent(DomainEventBase domainEvent) => _domainEvents.Add(domainEvent);
-  internal void ClearDomainEvents() => _domainEvents.Clear();
+public abstract class EntityBase<TId> : HasDomainEventsBase
+  where TId : struct, IEquatable<TId>
+{
+  public TId Id { get; set; }
 }
 
---
+++ src/Ardalis.SharedKernel/HasDomainEventsBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ardalis.SharedKernel;
+
+public abstract class HasDomainEventsBase
+{
+  private List<DomainEventBase> _domainEvents = new();
+  [NotMapped]
+  public IEnumerable<DomainEventBase> DomainEvents => _domainEvents.AsReadOnly();
+
+  protected void RegisterDomainEvent(DomainEventBase domainEvent) => _domainEvents.Add(domainEvent);
+  internal void ClearDomainEvents() => _domainEvents.Clear();
+
+}
+
